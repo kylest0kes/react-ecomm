31ad70e60651d6f830b9a20acc2664f8c297659a
kylest0kes <kylestokes1551@gmail.com>
2020-12-30 11:48:46 -0500

    created new grid for the view all page

--- src/components/ViewAllContainer/index.js
+++ src/components/ViewAllContainer/index.js
@@ -5,20 +5,24 @@ import './style.css';
 
 export default function ViewAllContainer() {
     return (
-        <div className="grid-x">
-            <div className="cell medium-4 large-2 grid-left">
+        <div className="grid-x all-container">
+            <div className="cell small-2 medium-3 large-2 grid-left">
                 <h1>fuck</h1>
                 <h1>fuck</h1>
                 <h1>fuck</h1>
                 <h1>fuck</h1>
             </div>
-            <div className="cell medium-8 large-10 grid-right">
+            <div className="cell small-6 medium-5 large-7 grid-mid">
                 <h1>fuck</h1>
                 <h1>fuck</h1>
                 <h1>fuck</h1>
                 <h1>fuck</h1>
                 <h1>fuck</h1>
-                
+            </div>
+            <div className="cell small-4 medium-4 large-3 grid-right">
+                <h1>fuck</h1>
+                <h1>fuck</h1>
+                <h1>fuck</h1>
             </div>
         </div>
     )
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -7,24 +7,7 @@ import './style.css';
 
 export default function Header() {
     return (
-        // <div className="top-bar-container header" data-sticky-container>
-        //     <div className="sticky sticky-topbar" data-sticky data-options="anchor: page; marginTop: 0; stickyOn: small;">
-        //         <div className="top-bar">
-        //             <div className="top-bar-left">
-        //                 <ul className="dropdown menu" data-dropdown-menu>
-        //                     <li className="header-logo"><img src={logo} alt="logo"/></li>
-        //                 </ul>
-        //             </div>
-        //             <div className="top-bar-right">
-        //                 <ul className="menu">
-        //                     <li><Link to="/register" className="button menu-btn"><button type="button" className="menu-btn-text">Register</button></Link></li>
-        //                     <li><Link to="/signin" className="button menu-btn"><button type="button" className="menu-btn-text">Sign-In</button></Link></li>
-        //                 </ul>
-        //             </div>
-        //         </div>
-        //     </div>
-        // </div>
-        <div className="top-bar-container header" data-sticky-container>
+        <div className="header" data-sticky-container>
             <div className="sticky sticky-topbar" data-sticky data-options="anchor: page; marginTop: 0; stickyOn: small;">
                 <div className="grid-x">
                     <div className="cell small-6 medium-6 large-10">
