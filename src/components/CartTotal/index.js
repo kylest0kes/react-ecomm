b15da2b6209b976b5893081aac27e5079cb6cf39
kylest0kes <kylestokes1551@gmail.com>
2021-01-02 11:43:22 -0500

    added changes to the buttons for checkout and clear cart

--- src/components/CartTotal/index.js
+++ src/components/CartTotal/index.js
@@ -17,8 +17,8 @@ export default function CartTotal() {
                     <p>$12.99</p>
                 </div>
             </div>
-            <button class="button expanded">Proceed to Checkout</button>
-            <button class="button expanded">Empty Your Cart</button>
+            <button class="button expanded proceed">Proceed to Checkout</button>
+            <button class="button expanded clear-cart">Empty Your Cart</button>
         </div>
     )
 }
