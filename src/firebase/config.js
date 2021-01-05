eb359ab3438aed5020ddfd9b2b9642e9f88d3da8
kylest0kes <kylestokes1551@gmail.com>
2021-01-04 19:24:55 -0500

    set up file structure for firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyA80sRFzIxlZLVjy61JXinCUZ9ZrVbOmec",
+    authDomain: "dminfinity-579b2.firebaseapp.com",
+    databaseURL: "https://dminfinity-579b2-default-rtdb.firebaseio.com",
+    projectId: "dminfinity-579b2",
+    storageBucket: "dminfinity-579b2.appspot.com",
+    messagingSenderId: "479922005575",
+    appId: "1:479922005575:web:dc56d135e6fe73b7cd3362",
+    measurementId: "G-JF99SWK8GK"
+};
